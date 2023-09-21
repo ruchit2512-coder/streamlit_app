97754720bf99f1df514f4aaaf0c2cf32eba68bef
ruchit2512-coder <79696628+ruchit2512-coder@users.noreply.github.com>
2023-09-21 12:31:46 +0530

    Update streamlit_app.py
    added some raw data from aws bucket and converted into a dataframe using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
+import pandas
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍞 bread jam | 🥣 omega 3 Blueberry')
 streamlit.text('🐔 egg omelette | 🥑 avacado')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
