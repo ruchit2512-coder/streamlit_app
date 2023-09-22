2ac5431e09b0d06a7b7b844e56a0decdf4c63dfa
ruchit2512-coder <79696628+ruchit2512-coder@users.noreply.github.com>
2023-09-22 17:06:36 +0530

    Update streamlit_app.py
    adding the user choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,15 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+
+u_choice = streamlit.text_input('What fruit would you like information about?','banana')
+streamlit.write('The user entered ', u_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+# streamlit.text(fruityvice_response.json())
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("insert into from fruit_load_list values(u_choice)")
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
