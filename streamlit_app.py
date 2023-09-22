27477ff41bde95493f5dcca3dd46154f0b4486d9
ruchit2512-coder <79696628+ruchit2512-coder@users.noreply.github.com>
2023-09-22 12:24:33 +0530

    Update streamlit_app.py
    adding input area

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('🍌🥭 Fruityvice Fruit Advice! 🥝🍇')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
